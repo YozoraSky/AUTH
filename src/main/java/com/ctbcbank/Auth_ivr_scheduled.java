c752455e9c607fa968b024fb1176ba502e1f2f55
YozoraSky <john.hsiao@hualiteq.com>
2019-09-17 14:49:34 +0800

    改動成: 5個授權批次由5個不同的時間開始執行

--- src/main/java/com/ctbcbank/Auth_ivr_scheduled.java
+++ src/main/java/com/ctbcbank/Auth_ivr_scheduled.java
@@ -1,7 +1,6 @@
 package com.ctbcbank;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,12 +19,28 @@ public class Auth_ivr_scheduled {
 	@Autowired
 	private Authpq28 authpq28;
 	
-	@Scheduled(cron="${auth.cron}")
-	public void run() {
-		authpq25.start();
-		authpq26.start();
-		authpq27.start();
-		authpq28.start();
+	@Scheduled(cron="${auth.authow05.cron}")
+	public void runAuthow05() {
 		authow05.start();
+	}
+	
+	@Scheduled(cron="${auth.authpq25.cron}")
+	public void runAuthpq25() {
+		authpq25.start();
+	}
+	
+	@Scheduled(cron="${auth.authpq26.cron}")
+	public void runAuthpq26() {
+		authpq26.start();
+	}
+	
+	@Scheduled(cron="${auth.authpq27.cron}")
+	public void runAuthpq27() {
+		authpq27.start();
+	}
+	
+	@Scheduled(cron="${auth.authpq28.cron}")
+	public void runAuthpq28() {
+		authpq28.start();
 	}
 }
